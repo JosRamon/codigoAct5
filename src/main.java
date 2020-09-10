0145313303315db36acdbbf0df28fe251b7a580a
JosRamon <ramon.martinez.m815@outlook.com>
2020-09-10 13:06:35 -0500

    version 2

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ public class calculadora {
     public static double resta (double a, double b) { return a-b; }
     public static double multiplica (double a, double b) { return a*b; }
     public static double divide (double a, double b) { return a/b; }
-
+    public static double modulo (double a, double b){ return a%b; }
 
     public static void main (String[]args) {
         Scanner entrada = new Scanner (System.in);
@@ -20,5 +20,6 @@ public class calculadora {
         System.out.println("La suma de ambos numeros es: "+suma(numero1,numero2));
         System.out.println("La resta de ambos numeros es: "+resta(numero1,numero2));
         System.out.println("La multiplicacion de ambos numeros es: "+multiplica(numero1,numero2));
-        System.out.println("La división de ambos numeros es: "+divide(numero1,numero2)); }
+        System.out.println("La división de ambos numeros es: "+divide(numero1,numero2));
+	System.out.println("El modulo de ambos numeros es: "+modulo(numero1,numero2)); }
 }
